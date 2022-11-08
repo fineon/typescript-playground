2387a660215708823827d2f25a66171c7f328558
ian <ian@heythereian.com>
2022-11-08 14:55:01 -0800

    init

---
+++ index.ts
@@ -0,0 +1,13 @@
+let dictionary = {
+    apple: 'fruit',
+    lettuce: 'vegetable'
+}
+
+interface dictionary {
+    apple: string;
+    lettuce: string;
+}
+
+function doSearch(db:dictionary) {
+    return null
+}
